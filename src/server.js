42df3ac36bb77f1c6263b3f2c78aed31bf0318a6
Mariam <mariam.antar@hiruep.com.au>
2020-04-05 16:53:54 +1000

    set up files with route

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const layout = require('express-layout');
+
+const routes = require('./routes');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+
+app.set('views', path.join(__direname, 'views'));
+app.set('view engine', 'ejs');
+
+const middlewares = [
+    layout(),
+    express.static(path.join(__dirname, 'public')),
+    bodyParser.urlencoded({extended: false})
+]
+
+app.use(middlewares)
+
+app.use('/', routes)
+
+app.listen(5001, () => {
+     console.log('App running on port 5001')
+})
